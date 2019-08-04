640607554617bd801df2760523c8b3d4f2067aaf
Med <mehdi.gacem@versusmind.eu>
2019-08-04 14:21:59 +0200

    create method consuming the service updating a hero

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -29,4 +29,10 @@ export class HeroDetailComponent implements OnInit {
   private goBack():void{
     this.location.back();
   }
+
+  public save() : void{
+    this.heroService.updateHero(this.hero).subscribe(
+      () => this.goBack()
+    )
+  }
 }
